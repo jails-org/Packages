2b960984954667b2d5f16213fb894c20fcb367b5
Eduardo Ottaviani Aragão <earagao@fbiz.com.br>
2017-04-12 19:11:40 -0300

    Finishing workflow for exporting packages

---
+++ webpack.config.babel.js
@@ -0,0 +1,33 @@
+import path from 'path'
+import glob from 'glob'
+import {optimize} from 'webpack'
+
+const uglioptions = { compress :{ warnings:false }, minimize :true }
+const uglify 	  = new optimize.UglifyJsPlugin( uglioptions )
+const plugins 	  = [uglify]
+
+const loaders = [{
+	loader: 'babel',
+	test: /\.js$/,
+	query:{
+		presets:['es2015']
+	}
+}]
+
+const library = (name)=>({
+	plugins,
+	entry 	:`./${name}/index.js`,
+	externals :{ 'jails-js' :'jails', 'morphdom' :'morphdom', 'page' :'page', 'axios' :'axios' },
+	module	:{ loaders },
+	output  :{
+		filename :`${name}/dist/index.js`,
+		libraryTarget:'umd',
+		library:name,
+		umdNamedDefine :true
+	}
+})
+
+export default glob.sync('./!(node_modules)/index.js').map( file =>{
+	let name = path.basename(path.dirname(file))
+	return library(name)
+})
