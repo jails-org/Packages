123eb2e596fdb5012f85994ce3dffa25672874b2
Javiani <edudu_tata@hotmail.com>
2017-10-14 03:45:29 -0300

    Updating reactor, veryfing if dom element and jails instance are in sync

--- reactor/index.js
+++ reactor/index.js
@@ -8,10 +8,14 @@ export {
 
 export default option => Base =>{
 
+	const id = Math.random()
+
+	Base.__reactorid = id
+	Base.elm.__reactorid = id
+
 	if( Base.elm == document.body ){
 		Base.reactor = state => { console.warn('Reactor can`t be used on document.body') }
-	}
-	else{
+	}else{
 		let template = Base.elm.querySelector('template')
 		let html = Base.elm.outerHTML
 
@@ -19,10 +23,21 @@ export default option => Base =>{
 			html = html.replace(/<template*.>/g, '').replace(/<\/template>/g, '')
 
 		Base.reactor = state => {
-			let status = {}
-			morphdom( Base.elm, soda(html, state), lifecycle( Base.elm, status ) )
-			if( status.hascomponent )
+
+			let status = { diffs:[] }
+
+			morphdom( Base.elm, soda( html, state ), lifecycle( Base.elm, status ) )
+
+			if( status.hascomponent ){
 				Base.jails.start( Base.elm )
+				status.hascomponent = false
+			}
+			status.diffs = status.diffs.forEach( n => {
+				Base.jails.destroy( n )
+				Base.jails.start( n.parentNode )
+				return false
+			})
+			status.diffs = []
 		}
 	}
 
@@ -33,17 +48,20 @@ export default option => Base =>{
 				return false
 		},
 
+		onElUpdated( node ){
+			const id = node.__reactorid
+			if(id && (id!= Base.__reactorid))
+				status.diffs.push(node)
+		},
+
 		onNodeAdded( node ) {
 			if( node.getAttribute && node.getAttribute('data-component') )
 				status.hascomponent = true
 		},
 
 		onBeforeNodeDiscarded( node ){
-			if( node.getAttribute ){
-				let name = node.getAttribute('data-component')
-				if( name )
-					Base.jails.destroy( node )
-			}
+			if( node.getAttribute && node.getAttribute('data-component') )
+				Base.jails.destroy( node )
 		}
 	})
 
