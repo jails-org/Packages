9ab1327462e9267972dd9ff269e359708e6e0214
Rodrigo Reis <br.rodrigoreis@gmail.com>
2021-05-26 12:24:58 -0300

    feat: get files when input is type file

--- form2/utils.js
+++ form2/utils.js
@@ -32,13 +32,21 @@ export const getFormData = (form) => {
 			if (el.name) {
 				const therules = (new Function(`return ${el.dataset.rules}`))()
 				const rules = therules ? therules : null
+
 				const isCheckbox = (el.type && (el.type == 'checkbox'))
-				const value = isCheckbox
-					? el.checked ? el.value : ''
-					: el.form[el.name].value
-				acc[el.name] = value
+				const isFile = (el.type && (el.type == 'file'))
+
+				if (isCheckbox && el.checked) {
+					return acc[el.name] = el.value
+				}
+
+				if (isFile && el.value) {
+					return acc[el.name] = el.files
+				}
+
+				acc[el.name] = el.value
+
 			}
-			return acc
 		}, {})
 
 	return data
